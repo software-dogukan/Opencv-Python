e7377e3e23979186eca5a71518c63f5140c37e9e
Dogukan Alkan <78702122+software-dogukan@users.noreply.github.com>
2024-08-08 16:32:07 +0300

    Image Gradients

---
+++ Opencv_Image_Gradients.py
@@ -0,0 +1,21 @@
+import numpy as np
+import cv2 as cv
+from matplotlib import pyplot as plt
+
+img = cv.imread('dave.jpg', cv.IMREAD_GRAYSCALE)
+assert img is not None, "file could not be read, check with os.path.exists()"
+
+laplacian = cv.Laplacian(img, cv.CV_64F)
+sobelx = cv.Sobel(img, cv.CV_64F, 1, 0, ksize=5)
+sobely = cv.Sobel(img, cv.CV_64F, 0, 1, ksize=5)
+
+plt.subplot(2, 2, 1), plt.imshow(img, cmap='gray')
+plt.title('Original'), plt.xticks([]), plt.yticks([])
+plt.subplot(2, 2, 2), plt.imshow(laplacian, cmap='gray')
+plt.title('Laplacian'), plt.xticks([]), plt.yticks([])
+plt.subplot(2, 2, 3), plt.imshow(sobelx, cmap='gray')
+plt.title('Sobel X'), plt.xticks([]), plt.yticks([])
+plt.subplot(2, 2, 4), plt.imshow(sobely, cmap='gray')
+plt.title('Sobel Y'), plt.xticks([]), plt.yticks([])
+
+plt.show()
