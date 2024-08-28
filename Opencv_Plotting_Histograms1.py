4d0f54615c20eb6560dc7c9b23b345690e583f16
Dogukan Alkan <78702122+software-dogukan@users.noreply.github.com>
2024-08-28 23:44:15 +0300

    Plotting

---
+++ Opencv_Plotting_Histograms1.py
@@ -0,0 +1,24 @@
+import numpy as np
+import cv2 as cv
+from matplotlib import pyplot as plt
+
+img = cv.imread('home.jpg', cv.IMREAD_GRAYSCALE)
+assert img is not None, "file could not be read, check with os.path.exists()"
+
+# create a mask
+mask = np.zeros(img.shape[:2], np.uint8)
+mask[100:300, 100:400] = 255
+masked_img = cv.bitwise_and(img, img, mask=mask)
+
+# Calculate histogram with mask and without mask
+# Check third argument for mask
+hist_full = cv.calcHist([img], [0], None, [256], [0, 256])
+hist_mask = cv.calcHist([img], [0], mask, [256], [0, 256])
+
+plt.subplot(221), plt.imshow(img, 'gray')
+plt.subplot(222), plt.imshow(mask, 'gray')
+plt.subplot(223), plt.imshow(masked_img, 'gray')
+plt.subplot(224), plt.plot(hist_full), plt.plot(hist_mask)
+plt.xlim([0, 256])
+
+plt.show()
